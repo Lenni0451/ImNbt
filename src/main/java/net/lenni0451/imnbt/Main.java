35ae924a731be0ec9472c4757f5e9f88a1c1314e
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2023-02-21 17:29:58 +0100

    Added version to main

--- src/main/java/net/lenni0451/imnbt/Main.java
+++ src/main/java/net/lenni0451/imnbt/Main.java
@@ -4,6 +4,8 @@ import imgui.app.Application;
 
 public class Main {
 
+    public static final String VERSION = "${version}";
+
     public static void main(String[] args) {
         Application.launch(new ImGuiImpl());
     }
