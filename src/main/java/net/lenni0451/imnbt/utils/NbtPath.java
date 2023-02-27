b08066794a215101b19b6c3b75f1ba04923ae15b
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2023-02-27 17:57:48 +0100

    Added more util methods

--- src/main/java/net/lenni0451/imnbt/utils/NbtPath.java
+++ src/main/java/net/lenni0451/imnbt/utils/NbtPath.java
@@ -22,6 +22,53 @@ public class NbtPath {
         return get(get(path, node), index);
     }
 
+    public static IPathNode[] parse(final String path) {
+        List<IPathNode> parts = new ArrayList<>();
+        StringBuilder currentPart = new StringBuilder();
+        char[] chars = path.toCharArray();
+        boolean escape = false;
+        boolean index = false;
+        for (char c : chars) {
+            if (c == ESCAPE.charAt(0)) {
+                escape = true;
+            } else if (escape) {
+                escape = false;
+                currentPart.append(c);
+            } else if (c == INDEX_START.charAt(0)) {
+                index = true;
+                parts.add(new PathNode(currentPart.toString()));
+                currentPart = new StringBuilder();
+            } else if (c == INDEX_END.charAt(0)) {
+                parts.add(new PathIndex(Integer.parseInt(currentPart.toString())));
+                currentPart = new StringBuilder();
+            } else if (c == PATH_SEPARATOR.charAt(0)) {
+                if (!index) parts.add(new PathNode(currentPart.toString()));
+                currentPart = new StringBuilder();
+                index = false;
+            } else {
+                currentPart.append(c);
+            }
+        }
+        if (!index) parts.add(new PathNode(currentPart.toString()));
+        return parts.toArray(new IPathNode[0]);
+    }
+
+    public static String build(final IPathNode[] nodes) {
+        String out = "";
+        for (int i = 0; i < nodes.length; i++) {
+            IPathNode node = nodes[i];
+            if (node instanceof PathNode path) {
+                if (i == 0) out = path.name();
+                else out = get(out, path.name());
+            } else if (node instanceof PathIndex index) {
+                out = get(out, index.index);
+            } else {
+                throw new IllegalArgumentException("Unknown node type: " + node.getClass().getName());
+            }
+        }
+        return out;
+    }
+
     public static String[] split(final String path) {
         List<String> parts = new ArrayList<>();
         StringBuilder currentPart = new StringBuilder();
@@ -42,6 +89,27 @@ public class NbtPath {
         }
         parts.add(currentPart.toString());
         return parts.toArray(new String[0]);
+    }
+
+
+    public interface IPathNode {
+        String name();
+    }
+
+    public record PathNode(String name) implements IPathNode {
+    }
+
+    public static class PathIndex implements IPathNode {
+        private final int index;
+
+        public PathIndex(final int index) {
+            this.index = index;
+        }
+
+        @Override
+        public String name() {
+            return INDEX_START + this.index + INDEX_END;
+        }
     }
 
 }
