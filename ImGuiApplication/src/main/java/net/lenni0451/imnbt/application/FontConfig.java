62ff08096079fde1586e508641de659c4d0173f2
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2023-07-24 22:00:01 +0200

    Save font size between restarts

--- ImGuiApplication/src/main/java/net/lenni0451/imnbt/application/FontConfig.java
+++ ImGuiApplication/src/main/java/net/lenni0451/imnbt/application/FontConfig.java
@@ -3,10 +3,15 @@ package net.lenni0451.imnbt.application;
 import imgui.ImFont;
 import net.lenni0451.imnbt.FontHandler;
 
+import java.util.prefs.Preferences;
+
 public class FontConfig implements FontHandler {
 
+    private static final String KEY_NAME = "selectedFont";
+
+    private final Preferences preferences = Preferences.userNodeForPackage(this.getClass());
     private final ImFont[] fonts = new ImFont[5];
-    private int selectedFont = 3;
+    private int selectedFont = Math.max(0, Math.min(this.preferences.getInt(KEY_NAME, 3), this.fonts.length - 1));
 
     @Override
     public ImFont[] getFonts() {
@@ -25,6 +30,7 @@ public class FontConfig implements FontHandler {
     @Override
     public void setSelectedFont(int selectedFont) {
         this.selectedFont = selectedFont;
+        this.preferences.putInt(KEY_NAME, selectedFont);
     }
 
 }
