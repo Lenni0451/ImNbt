3ef3f6c4b46c910f722fb886b69884674b2af65a
Lenni0451 <20379977+Lenni0451@users.noreply.github.com>
2025-02-22 17:57:05 +0100

    Disable first thread check on macos

--- ImGuiApplication/src/main/java/net/lenni0451/imnbt/application/Main.java
+++ ImGuiApplication/src/main/java/net/lenni0451/imnbt/application/Main.java
@@ -1,6 +1,7 @@
 package net.lenni0451.imnbt.application;
 
 import imgui.app.Application;
+import org.lwjgl.system.Configuration;
 
 public class Main {
 
@@ -21,6 +22,9 @@ public class Main {
     private final ImGuiImpl imGuiImpl = new ImGuiImpl(this.config, this.fontConfig);
 
     private void init() {
+        if (System.getProperty("os.name").toLowerCase().contains("mac")) {
+            Configuration.GLFW_CHECK_THREAD0.set(false);
+        }
         Application.launch(this.imGuiImpl);
     }
 
